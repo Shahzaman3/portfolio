0e54413fdaa0e52afbd69e4376304785f72848df
Shahzaman3 <faisalshahzaman8@gmail.com>
2025-04-26 10:12:51 +0530

    media path changed

--- src/pages/Page4.jsx
+++ src/pages/Page4.jsx
@@ -6,7 +6,7 @@ export const Page4 = () => {
       return ( 
             <div className="h-screen p-8 ">
                   <div className="h-full w-full overflow-hidden relative rounded-[30px] flex items-center justify-between">
-                        <video autoPlay loop muted className="h-full w-full object-cover" src="/public/skill.mp4"></video>
+                        <video autoPlay loop muted className="h-full w-full object-cover" src="/skill.mp4"></video>
                         <div className="absolute flex flex-col md:flex-row items-start md:items-end gap-4 left-15 bottom-0 p-8">
                               <a className="text-[28vw] font-[anzo2] text-white" href="http://" target="_blank" rel="noopener noreferrer">SKILLS</a>
                         </div>
